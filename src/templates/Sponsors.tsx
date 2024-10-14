0225d99a21f7cca5edf76ac516257c8be94ce503
Mark Valles <mvalles36@users.noreply.github.com>
2024-10-14 14:42:35 -0500

    Update Sponsors.tsx

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -6,50 +6,50 @@ import { Section } from '@/features/landing/Section';
 
 export const Sponsors = () => (
   <Section>
-    <LogoCloud text="Sponsored by">
+    <LogoCloud text="Our Partners">
       <a
-        href="https://clerk.com?utm_source=github&utm_medium=sponsorship&utm_campaign=nextjs-boilerplate"
+        href=""
         target="_blank"
         rel="noopener"
       >
         <Image
           src="/assets/images/clerk-logo-dark.png"
-          alt="Clerk"
+          alt="Titan"
           width="128"
           height="37"
         />
       </a>
 
       <a
-        href="https://l.crowdin.com/next-js"
+        href=""
         target="_blank"
         rel="noopener"
       >
         <Image
           src="/assets/images/crowdin-dark.png"
-          alt="Crowdin"
+          alt="GAF"
           width="128"
           height="26"
         />
       </a>
 
       <a
-        href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo"
+        href=""
         target="_blank"
         rel="noopener"
       >
         <Image
           src="/assets/images/sentry-dark.png"
-          alt="Sentry"
+          alt="Home Depot"
           width="128"
           height="38"
         />
       </a>
 
-      <a href="https://nextjs-boilerplate.com/pro-saas-starter-kit">
+      <a href="">
         <Image
           src="/assets/images/nextjs-boilerplate-saas.png"
-          alt="Nextjs SaaS Boilerplate"
+          alt="Lowes"
           width="128"
           height="30"
         />
